cb96d32a7d5f964b9164131e6a72ad842f9c38ba
artUSUN <a.d.levadniy@gmail.com>
2020-06-27 22:43:09 +0300

    Сделал и подогнал платформы. GameField настроен и работает. Написал скрипт спавна и кручения мира. Пока без реакции на игрока

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+
+    public Transform Begin;
+    public Transform End;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameWorldController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWorldController : MonoBehaviour
+{
+    public GameObject startChunk;
+    [SerializeField] private Vector3 gameFieldRotationSpeed;
+
+    [SerializeField] private GameObject[] chunkPrefabs;
+
+    private List<GameObject> spawnedChunks = new List<GameObject>();
+    private Transform centerOfGameField;
+    private Transform player;
+
+    void Start()
+    {
+        centerOfGameField = transform;
+        player = GameObject.Find("Player").transform;
+        spawnedChunks.Add(startChunk);
+    }
+
+    
+    void Update()
+    {
+        GameFieldRotation();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SpawnChunk(chunkPrefabs[0]);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SpawnChunk(chunkPrefabs[1]);
+        }
+    }
+
+    private void SpawnChunk(GameObject chunkPrefab)
+    {
+        GameObject newChunk = Instantiate(chunkPrefab, centerOfGameField.position, Quaternion.identity, centerOfGameField);
+
+        newChunk.transform.rotation = spawnedChunks[spawnedChunks.Count - 1].transform.rotation * Quaternion.Euler(5f, 0f, 0f);
+
+        spawnedChunks.Add(newChunk);
+
+        //newChunk.transform.position = spawnedChunks[spawnedChunks.Count - 1].End.position - newChunk.Begin.localPosition;
+        //newChunk.transform.Rotate()
+    }
+
+    private void GameFieldRotation()
+    {
+        centerOfGameField.Rotate(gameFieldRotationSpeed * Time.deltaTime);
+    }
+}
+
